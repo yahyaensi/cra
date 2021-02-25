9e0d9cd222031bb84a343b2fae9fdb2dfe7dabc1
Yahya <yahya.romdhane.ensi@gmail.com>
2021-02-25 22:56:45 +0100

    add config

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const { ESLINT_MODES } = require("@craco/craco");
 
 module.exports = {
-    // ...
+	eslint: {
+		mode: ESLINT_MODES.extends,
+		configure: () => {
+			// Workaround for broken ESLINT_MODES.file mode
+			return require('./.eslintrc')
+		}
+	},
     webpack: {
         alias: {
-            '': path.join(path.resolve(__dirname, './src')),
+            'components': path.join(path.resolve(__dirname, './src/components')),
+			'containers': path.join(path.resolve(__dirname, './src/containers')),
+			'redux/actions': path.join(path.resolve(__dirname, './src/actions')),
+			'redux/reducers': path.join(path.resolve(__dirname, './src/reducers')),
+			'router': path.join(path.resolve(__dirname, './src/router')),
         }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import { useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from '@/components/TestComponent';
+import TestComponent from 'components/TestComponent';
 
-function messageHandler(event) {
-	if (event && event.data === 'Authentication succeeded') {
-		// refresh imti iframe after Authentication success
-		let iframe = document.getElementById('imtiIframe');
-		iframe.src = iframe.src;
-	}
-}
 
 function App() {
-	
-  useEffect(() => {
-		window.addEventListener("message", messageHandler, false);
 
-		return function cleanup() {
-			window.removeEventListener("message", messageHandler, false)
-		}
-  });
 
   return (
     <div className="App">
-		yahya
+		<TestComponent />
     </div>
   );
 }
